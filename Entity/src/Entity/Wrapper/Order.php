ddddd2b073610ceb7ef8d01e58b5a716c1285ef4
gelysis <andreas@gelysis.net>
2014-06-20 09:43:46 +1200

    Reverted legacy coming from dev

--- Entity/src/Entity/Wrapper/Order.php
+++ Entity/src/Entity/Wrapper/Order.php
@@ -133,116 +133,33 @@ class Order extends AbstractWrapper
     }
 
     /**
-     * Returns array of Payment Methods to be extracted as values
-     * @return string[]
+     * @return array|null|string
      */
-    public function getRawPaymentMethods()
+    public function getPayments()
     {
-        $paymentMethods = $this->getData('payment_method');
-
-        if (is_array($paymentMethods)) {
-            if (count($paymentMethods) <= 1) {
-                $paymentMethodsArray[] = trim(  array_shift( array_keys($paymentMethods) )  );
-            } else {
-                foreach ( array_keys($paymentMethods) as $key ) {
-                    $paymentMethodsArray[] = trim($key);
-                }
-            }
-            return $paymentMethodsArray;
-        } else {
-            return array();
-        }
+        return $this->getData('payment_method');
     }
 
-    /*use Node\AbstractNode;*/
-    /*use Node\Entity;*/
-/*    public function getRawOutput() {
-
-        protected $_node;
-        protected $_nodeEnt;
-        protected $_soap = null;
-        protected $_db = null;
-        protected $_ns = null;
-
-        $this->_node = $node;
-        $this->_nodeEnt = $nodeEntity;
-
-        $this->_soap = $node->getApi('soap');
-        if(!$this->_soap){
-            throw new MagelinkException('SOAP is required for Magento Orders');
-        }
-        $this->_db = $node->getApi('db');
-
-        $this->_ns = $this->getServiceLocator()->get('nodeService');
-
-        if($this->_soap) {
-            $results2 = $this->_soap->call('salesOrderList', array(
-                array(
-                    'complex_filter'=>array(
-                        array(
-                            'key'=>'updated_at',
-                            'value'=>array('key'=>'gt', 'value'=>$retTime),
-                             ),
-                        ),
-                    ),
-                ));
-        }
-
-        foreach ($results2 as $orderFromList) {
-            $order2 = $this->_soap->call('salesOrderInfo', array($orderFromList['increment_id']));
-            if (isset($order2['result'])) {
-                $order2 = $order2['result'];
-            }
-            foreach(array_diff(array_keys($orderFromList), array_keys($order2)) as $key) {
-                $order2[$key] = $orderFromList[$key];
-            }
-        }
-        return $order2;
-    }*/
-
-
-/*
+    /**
+     * @return mixed
+     */
     public function getPaymentMethods()
     {
-        $paymentMethods = $this->getData('payment_method');
+        /** @var \Entity\Service\EntityService */
+        $entityService = $this->getServiceLocator()->get('entityService');
 
-        if ( is_array($paymentMethods) ) {
-            return trim(  array_shift( array_keys($paymentMethods) )  );
-        } else {
-            return false;
-        }
+        return $entityService->getPaymentMethods($this);
     }
 
-    public function getCreditCardBool()
+    /**
+     * @return mixed
+     */
+    public function getPaymentCcTypes()
     {
-        $ccInfo = $this->getData('payment_method');
+        /** @var \Entity\Service\EntityService */
+        $entityService = $this->getServiceLocator()->get('entityService');
 
-        if ( is_array($ccInfo) ) {
-            return preg_match(   '/.+{{\w+}}/i', trim(  array_shift( array_keys($ccInfo) )  )   ) ? true : false;
-        } else {
-            return false;
-        }
+        return $entityService->getPaymentCcTypes($this);
     }
 
-    public function getCreditCardInfo()
-    {
-        $ccInfo = $this->getData('payment_method');
-
-        if ( is_array($ccInfo) && $this->getCreditCardBool() ) {
-            return trim(  array_shift( array_keys($ccInfo) )  );
-        } else {
-            return '';
-        }
-    }
-
-    public function getPaymentAmounts()
-    {
-        $paymentMethods = $this->getData('payment_method');
-
-        if ( is_array($paymentMethods) ) {
-            return $paymentMethods;
-        } else {
-            return array();
-        }
-    }*/
 }
