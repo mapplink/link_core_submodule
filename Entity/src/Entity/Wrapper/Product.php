33c181f97fc030f631c06b03d10936ff55c3203b
gelysis <andreas@gelysis.net>
2014-07-15 08:40:58 +1200

    Cleanup (empty class)

--- Entity/src/Entity/Wrapper/Product.php
+++ Entity/src/Entity/Wrapper/Product.php
@@ -1,24 +1,16 @@
 <?php
 /**
- * HOPS
- *
- * @category    HOPS
- * @author      Sean Yao <sean@lero9.com>
- * @copyright   Copyright (c) 2014 LERO9 Ltd.
- * @license     Commercial - All Rights Reserved
+ * Product entity
+ * @category HOPS
+ * @author Seo Yao
+ * @author Andreas Gerhards <andreas@lero9.co.nz>
+ * @copyright Copyright (c) 2014 LERO9 Ltd.
+ * @license Commercial - All Rights Reserved
  */
-
 
 namespace Entity\Wrapper;
 
 use Entity\Entity;
 
-/**
- * Product entity
- */
-class Product extends AbstractWrapper
-{
 
-
-
-}
+class Product extends AbstractWrapper {}
