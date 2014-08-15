bcfcf04fcdbd54251e6138f4a3407378159de9e7
gelysis <andreas@gelysis.net>
2014-08-15 12:17:41 +1200

    Extended cron file with classes to use for the monitoring system

--- Application/src/Application/Controller/Cron.php
+++ Application/src/Application/Controller/Cron.php
@@ -19,50 +19,83 @@ use Magelink\Exception\MagelinkException;
 class Cron extends AbstractActionController implements ServiceLocatorAwareInterface
 {
 
+    const LOCKS_DIRECTORY = 'data/locks';
+
+    /**
+     * Get file name of cronjob
+     * @param $code
+     * @return string
+     */
+    public static function getLockFileName($code)
+    {
+        $fileName = self::LOCKS_DIRECTORY.'/'.bin2hex(crc32('cron-'.$code)).'.lock';
+        return $fileName;
+    }
+
+    /**
+     * Check if cronjob is unlocked
+     * @param $code
+     * @return bool
+     */
+    public static function checkIfUnlocked($code)
+    {
+        $fileName = self::getLockFileName($code);
+        if (file_exists($fileName)) {
+            $unlocked = FALSE;
+        }else{
+            $unlocked = TRUE;
+        }
+
+        return $unlocked;
+    }
+
     /**
      * Acquire an exclusive lock for the provided lock codename
      * @param $code
      * @return bool
      * @throws \Magelink\Exception\MagelinkException
      */
-    protected function acquireLock($code){
-        if(!is_dir('data/locks')){
-            mkdir('data/locks');
+    protected function acquireLock($code)
+    {
+        if (!is_dir(self::LOCKS_DIRECTORY)) {
+            mkdir(self::LOCKS_DIRECTORY);
         }
-        if(!is_writable('data/locks')){
+        if (!is_writable(self::LOCKS_DIRECTORY)){
             throw new MagelinkException('Lock directory not writable!');
         }
-        $filename = 'data/locks/' . bin2hex(crc32($code)) . '.lock';
 
-        if(file_exists($filename)){
-            return false;
-        }else{
-            $hdl = fopen($filename, 'x');
-            if(!$hdl){
-                return false;
+        $unlocked = self::checkIfUnlocked($code);
+
+        if ($unlocked){
+            $fileName = self::getLockFileName($code);
+            $handle = fopen($fileName, 'x');
+
+            if (!$handle) {
+                $unlocked = FALSE;
             }else{
-                $res = flock($hdl, LOCK_EX | LOCK_NB);
-                if(!$res){
-                    return false;
+                $res = flock($handle, LOCK_EX | LOCK_NB);
+                if (!$res){
+                    $unlocked = FALSE;
+                }else{
+                    fwrite($handle, $code.'; '.time().'; Date: '.date('Y-m-d H:i:s'));
+                    fflush($handle);
+                    flock($handle, LOCK_UN);
+                    fclose($handle);
                 }
-                fwrite($hdl, $code.', date: '.date('Y-m-d H:i:s'));
-                fflush($hdl);
-                flock($hdl, LOCK_UN);
-                fclose($hdl);
-                return true;
             }
         }
-        return false;
+
+        return $unlocked;
     }
 
     /**
      * Release an exclusive lock for the provided lock codename. NOTE: Does not check if we have the lock, simply unlocks, so can be dangerous.
      * @param $code
      */
-    protected function releaseLock($code){
-        $filename = 'data/locks/' . bin2hex(crc32($code)) . '.lock';
-
-        unlink($filename);
+    protected function releaseLock($code)
+    {
+        $fileName = self::getLockFileName($code);
+        unlink($fileName);
     }
 
     public function indexAction()
@@ -114,7 +147,7 @@ class Cron extends AbstractActionController implements ServiceLocatorAwareInterf
                 continue;
             }
 
-            $ran = true;
+            $ran = TRUE;
 
             $obj = new $class();
             if(!($obj instanceof \Application\CronRunnable)){
@@ -126,31 +159,50 @@ class Cron extends AbstractActionController implements ServiceLocatorAwareInterf
             
             $check = $obj->cronCheck($time);
             if($job == $name){
-                $check = true;
+                $check = TRUE;
                 // Forcing run for forced name
             }
             if(!$check){
-                $this->getServiceLocator()->get('logService')->log(\Log\Service\LogService::LEVEL_INFO, 'cron_skip', 'Skipping cron job ' . $name, array('time'=>$time, 'name'=>$name, 'class'=>$class));
+                $this->getServiceLocator()->get('logService')
+                    ->log(\Log\Service\LogService::LEVEL_INFO,
+                        'cron_skip',
+                        'Skipping cron job '.$name,
+                        array('time'=>$time, 'name'=>$name, 'class'=>$class)
+                    );
                 continue;
             }else{
-                $lock = $this->acquireLock('cron-'.$name);
-                if(!$lock){
-                    $this->getServiceLocator()->get('logService')->log(\Log\Service\LogService::LEVEL_ERROR, 'cron_locked', 'Locked cron job ' . $name, array('time'=>$time, 'name'=>$name, 'class'=>$class));
+                $lock = $this->acquireLock($name);
+                if (!$lock) {
+                    $this->getServiceLocator()->get('logService')
+                        ->log(\Log\Service\LogService::LEVEL_ERROR,
+                            'cron_locked',
+                            'Locked cron job '.$name,
+                            array('time'=>$time, 'name'=>$name, 'class'=>$class)
+                        );
                     continue;
                 }
-                $this->getServiceLocator()->get('logService')->log(\Log\Service\LogService::LEVEL_DEBUG, 'cron_run', 'Running cron job ' . $name, array('time'=>$time, 'name'=>$name, 'class'=>$class));
+                $this->getServiceLocator()->get('logService')
+                    ->log(\Log\Service\LogService::LEVEL_DEBUG,
+                        'cron_run',
+                        'Running cron job '.$name,
+                        array('time'=>$time, 'name'=>$name, 'class'=>$class)
+                    );
                 $obj->cronRun();
-                $this->releaseLock('cron-'.$name);
+                $this->releaseLock($name);
             }
-            
         }
 
-        if(!$ran && $job !== null){
-            $this->getServiceLocator()->get('logService')->log(\Log\Service\LogService::LEVEL_ERROR, 'cron_notfound', 'Could not find requested cron job ' . $job, array('job'=>$job));
+        if (!$ran && $job !== NULL) {
+            $this->getServiceLocator()->get('logService')
+                ->log(\Log\Service\LogService::LEVEL_ERROR,
+                    'cron_notfound',
+                    'Could not find requested cron job '.$job,
+                    array('job'=>$job)
+                );
         }
 
-        $this->getServiceLocator()->get('logService')->log(\Log\Service\LogService::LEVEL_INFO, 'cron_done', 'Cron completed', array('time'=>$time));
+        $this->getServiceLocator()->get('logService')
+            ->log(\Log\Service\LogService::LEVEL_INFO, 'cron_done', 'Cron completed', array('time'=>$time));
         die();
-        
     }
 }
