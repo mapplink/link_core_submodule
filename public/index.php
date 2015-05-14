77d86931339caa558c5ef3a98e8d005253c61cef
gelysis <andreas@gelysis.net>
2015-05-14 13:18:08 +1200

    Syntax/code tweaks of index.php/local.php

--- public/index.php
+++ public/index.php
@@ -3,11 +3,10 @@ if (file_exists('local.php')) {
     require_once('local.php');
 }
 
-// Everything is relative to the application root now
 chdir(dirname(str_replace('magelink/', '', __DIR__)));
 
 // Decline static file requests back to the PHP built-in webserver
-if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+if (php_sapi_name() === 'cli-server' && is_file(__DIR__.parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
     return FALSE;
 }
 
@@ -15,7 +14,7 @@ if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['RE
 require 'init_autoloader.php';
 
 require_once('magelink/Application/src/Application/Helper/ErrorHandler.php');
-$eh = new Application\Helper\ErrorHandler(FALSE);
+$errorHandler = new Application\Helper\ErrorHandler(FALSE);
 
 // Run the application!
 Zend\Mvc\Application::init(require 'config/application.config.php')->run();
