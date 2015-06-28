6b95c5ba87867bac95b692133a01adf4f8741017
gelysis <andreas@gelysis.net>
2015-06-29 11:11:21 +1200

    Cleanup on checking how to display default value on empty field

--- Web/src/Web/Controller/CRUD/NodeAdminController.php
+++ Web/src/Web/Controller/CRUD/NodeAdminController.php
@@ -1,12 +1,11 @@
 <?php
 /**
- * Web\Controller
- *
- * @category    Web
- * @package     Web\Controller
- * @author      Sean Yao <sean@lero9.com>
- * @copyright   Copyright (c) 2014 LERO9 Ltd.
- * @license     Commercial - All Rights Reserved
+ * @category Web
+ * @package Web\Controller
+ * @author Sean Yao
+ * @author Andreas Gerhards <andreas@lero9.co.nz>
+ * @copyright Copyright (c) 2014 LERO9 Ltd.
+ * @license Commercial - All Rights Reserved
  */
 
 namespace Web\Controller\CRUD;
@@ -16,52 +15,60 @@ use Web\Controller\CRUD\AbstractCRUDController;
 
 class NodeAdminController extends AbstractCRUDController
 {
+
     /**
      * Child classes should override to return the Entity class name that this CRUD controller works on.
      * @return string
      */
-    protected function getEntityClass(){
+    protected function getEntityClass()
+    {
         return 'Node\Entity\Node';
     }
 
+    /**
+     * @return array $listViewConfig
+     */
     protected function getListViewConfig()
     {
         return array(
-            'Id'           => array('linked' => true),
-            'Name'         => array('linked' => true),
-            'Type'         => array(),
+            'Id'=>array('linked'=>TRUE),
+            'Name'=>array('linked'=>TRUE),
+            'Type'=>array()
         );
     }
 
     /**
      * Set Filter Config
+     * @return array
      */
     protected function getSearchFilterConfig()
     {
         return array(
-            'name' => array(
-                'operators' => array('contains', 'equals'),
-                'label'     => 'Name',
-                'field'     => 'name',
+            'name'=>array(
+                'operators'=>array('contains', 'equals'),
+                'label'=>'Name',
+                'field'=>'name',
             ),
-            'type' => array(
-                'operators' => array('contains', 'equals'),
-                'label'     => 'Type',
-                'field'     => 'type',
-            ),
-            
+            'type'=>array(
+                'operators'=>array('contains', 'equals'),
+                'label'=>'Type',
+                'field'=>'type',
+            )
         );
     }
 
-     /**
-    * Get form
-    * @param  object $object 
-    * @return Zend/Form/Form
-    */
+    /**
+     * Get form
+     * @param  object $object
+     * @return Zend/Form/Form $form
+     */
     protected function getForm($object)
     {
         $classRelection = new \ReflectionClass($this->formClassName);
-        $form = $classRelection->newInstanceArgs(array($this->getEntityManager(), $this->getServiceLocator()->get('Config')));
+        $form = $classRelection->newInstanceArgs(array(
+            $this->getEntityManager(),
+            $this->getServiceLocator()->get('Config')
+        ));
         $form->bind($object);
 
         return $form;
