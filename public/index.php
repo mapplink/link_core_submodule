bf6e9b3c2b99fde6bf965d5e817e5530a91d91a3
gelysis <andreas@gelysis.net>
2015-07-09 15:26:51 +1200

    Tweaked chdir

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ if (file_exists('local.php')) {
     require_once('local.php');
 }
 
-chdir(dirname(str_replace('magelink/', '', __DIR__)));
+chdir(__DIR__.'/../..');
 
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server' && is_file(__DIR__.parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
