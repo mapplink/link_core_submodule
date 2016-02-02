25602b85a1259ca7f1e004161a51c07fdf06ef1a
gelysis <andreas@gelysis.net>
2016-02-02 14:29:28 +1300

    Changed packing screen order and added separated tracking code field

--- public/js/hops.js
+++ public/js/hops.js
@@ -264,13 +264,16 @@ var OrderAdminPacking = {
             return OrderAdminPacking.ajaxOrderCall();
         });
 
-        $('#userId').keypress(function(event) { if (event.which == 13) { return OrderAdminPacking.userIdNext(); }});
-
         $('#code').keypress(function(event) { if (event.which == 13) { return OrderAdminPacking.codeNext(); }});
         $('#code').blur(function() { return OrderAdminPacking.codeNext(); });
 
+        $('#userId').keypress(function(event) { if (event.which == 13) { return OrderAdminPacking.userIdNext(); }});
+
         $('#weight').keypress(function(event) { if (event.which == 13) { return OrderAdminPacking.weightNext(); }});
         $('#weight').blur(function() { return OrderAdminPacking.weightNext(); });
+
+        $('#tracking_code').keypress(function(event) { if (event.which == 13) { return OrderAdminPacking.trackingNext(); }});
+        $('#tracking_code').blur(function() { return OrderAdminPacking.trackingNext(); });
     },
 
     ajaxOrderCall: function() {
@@ -282,8 +285,8 @@ var OrderAdminPacking = {
             if (data.success) {
                 OrderAdminPacking.hideOrderComment();
                 OrderAdminPacking.setUseApi(data.useApi);
-                OrderAdminPacking.setCodeLabel(data.codeLabel);
                 OrderAdminPacking.setEnterWeight(data.enterWeight);
+                OrderAdminPacking.setEnterTracking(data.enterTracking);
             }
         })
         .fail(function(data) {
@@ -348,31 +351,49 @@ var OrderAdminPacking = {
             $('#enterWeight').val(0);
         }
     },
+    setEnterTracking: function(enterTracking) {
+        if (enterTracking) {
+            $('#group-tracking').show();
+            $('#enterTracking').val('On');
+        }else{
+            $('#group-tracking').hide();
+            $('#enterTracking').val(0);
+        }
+    },
     orderIdStay: function() {
         $('#orderId').val('');
         $('#orderId').focus();
         return false;
     },
     orderIdNext: function() {
-        $('#userId').val('');
-        $('#userId').focus();
-        return false;
-    },
-    userIdNext: function() {
         $('#code').val('');
         $('#code').focus();
         return false;
     },
     codeNext: function() {
+        $('#userId').val('');
+        $('#userId').focus();
+        return false;
+    },
+    userIdNext: function() {
         $('#weight').val('');
         if ($('#enterWeight').val() == 'On') {
             $('#weight').focus();
         }else{
-            $('#completeField').focus();
+            OrderAdminPacking.weightNext();
         }
         return false;
     },
     weightNext: function() {
+        $('#tracking_code').val('');
+        if ($('#enterTracking').val() == 'On') {
+            $('#tracking_code').focus();
+        }else{
+            OrderAdminPacking.trackingNext();
+        }
+        return false;
+    },
+    trackingNext: function() {
         $('#completeField').focus();
         return false;
     }
