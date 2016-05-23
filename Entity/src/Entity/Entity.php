614d45b9486a73621d7b27f0cf9cb7b94fc7df11
gelysis <andreas@gelysis.net>
2016-05-24 11:24:57 +1200

    Major cleanup; prepared deprecation of getEavService

--- Entity/src/Entity/Entity.php
+++ Entity/src/Entity/Entity.php
@@ -26,34 +26,34 @@ class Entity implements ServiceLocatorAwareInterface
     protected $_unique_id;
     protected $_updated_at;
     protected $_parent_id;
-    
-    /** @var array An array of all attribute data arrays that are applicable to this Entity
-     *     - note will likely only contain those for the Node it was loaded for
-     */
+
+    /** @var array $this->_attributes An array of all attribute data arrays that are applicable to this Entity
+     *                                - Note: Will likely only contain those for the Node it was loaded for */
     protected $_attributes = array();
-
-    /** @var array An array of each attributes fetch data, indexed by attribute code */
+    /** @var array $this->_attributesFetchData  An array of each attributes fetch data, indexed by attribute code */
     protected $_attributesFetchData = array();
-
-    /** @var array Map where key is attribute code and value is attribute ID */
+    /** @var array $this->_attributesMap  Map where key is attribute code and value is attribute ID */
     protected $_attributesMap = array();
 
-    /** @var int The node that this entity was loaded for */
-    protected $_loadedFromNode = false;
-    
-    /** @var array All loaded data for this Entity */
-    protected $_data = array();
+    /** @var int $this->_loadedFromNode */
+    protected $_loadedFromNode = FALSE;
 
-    /** @var array All extended data for this Entity (fkey data, etc) */
+    /** @var array $this->_data  All loaded data for this Entity */
+    protected $_data = array();
+    /** @var array $this->_extendedData  All extended data for this Entity (fkey data, etc) */
     protected $_extendedData = array();
 
-    /** @var \Entity\Entity[] */
+    /** @var \Entity\Entity[] $this->_resolveCache */
     protected $_resolveCache = array();
+    /** @var \Entity\Entity $this->_parentCache */
+    protected $_parentCache = FALSE;
+    /** @var \Entity\Entity $this->_childrenCache */
+    protected $_childrenCache = array();
 
-    /** @var ServiceLocatorInterface The service locator */
+    /** @var ServiceLocatorInterface $this->_serviceLocator */
     protected $_serviceLocator;
-    /** @var EntityService $_entityService */
-    protected $_entityService;
+    /** @var EntityService $this->_entityService */
+    protected $_entityService = NULL;
 
 
     /**
@@ -84,13 +84,25 @@ class Entity implements ServiceLocatorAwareInterface
     }
 
     /**
+     * @return \Entity\Service\EntityService $entityService
+     */
+    protected function getEntityService()
+    {
+        if (is_null($this->_entityService)) {
+            $this->setEntityService();
+        }
+
+        return $this->_entityService;
+    }
+
+    /**
+     * @deprecated Replace this with getEntityService() from version 3.2
      * @return \Entity\Service\EntityService
      */
     protected function getEavService()
     {
-        return $this->getServiceLocator()->get('entityService');
+        return $this->getEntityService();
     }
-
 
     /**
      * Creates a new Entity instance based on the provided row of data from the entity table.
@@ -134,14 +146,14 @@ class Entity implements ServiceLocatorAwareInterface
      * Add new attributes to the internal map - used for enhanceEntity primarily. Should not be used by end-user code.
      * @param $attData
      */
-    public function addAttribute($attData){
-        if(isset($this->_attributesMap[$attData['code']])){
-            return; // Already added
+    public function addAttribute($attData)
+    {
+        if (!isset($this->_attributesMap[$attData['code']])) {
+            $this->_attributes[$attData['attribute_id']] = $attData;
+            $this->_attributesMap[$attData['code']] = $attData['attribute_id'];
+            $this->_attributesFetchData[$attData['code']] =
+                $attData['fetch_data'] != NULL ? unserialize($attData['fetch_data']) : array();
         }
-        $this->_attributes[$attData['attribute_id']] = $attData;
-        $this->_attributesMap[$attData['code']] = $attData['attribute_id'];
-        $this->_attributesFetchData[$attData['code']] =
-            $attData['fetch_data'] != null ? unserialize($attData['fetch_data']) : array();
     }
 
     /**
@@ -149,7 +161,8 @@ class Entity implements ServiceLocatorAwareInterface
      * @param array $data Raw data - key is attribute ID value is data
      * @throws \Magelink\Exception\MagelinkException If values are passed that are not already loaded as attributes
      */
-    public function populateRaw($data){
+    public function populateRaw($data)
+    {
         foreach($data as $attribute_id=>$data){
             if(!isset($this->_attributes[$attribute_id])){
                 throw new MagelinkException('Invalid attribute data loaded ' . $attribute_id);
@@ -162,7 +175,8 @@ class Entity implements ServiceLocatorAwareInterface
      * Returns entity id
      * @return int
      */
-    public function getId() {
+    public function getId()
+    {
         return $this->_id;
     }
 
@@ -170,7 +184,8 @@ class Entity implements ServiceLocatorAwareInterface
      * Returns entity id (alias of getId())
      * @return int
      */
-    public function getEntityId() {
+    public function getEntityId()
+    {
         return $this->getId();
     }
 
@@ -178,7 +193,8 @@ class Entity implements ServiceLocatorAwareInterface
      * Return the entity type for this entity.
      * @return int
      */
-    public function getType(){
+    public function getType()
+    {
         return $this->_type;
     }
 
@@ -186,7 +202,8 @@ class Entity implements ServiceLocatorAwareInterface
      * Return the entity type "name" directly.
      * @return string
      */
-    public function getTypeStr(){
+    public function getTypeStr()
+    {
         return $this->getServiceLocator()->get('entityConfigService')->parseEntityTypeReverse($this->_type);
     }
 
@@ -194,7 +211,8 @@ class Entity implements ServiceLocatorAwareInterface
      * Return the store ID assigned to this entity (or 0 if global)
      * @return int
      */
-    public function getStoreId() {
+    public function getStoreId()
+    {
         return $this->_store_id;
     }
 
@@ -202,7 +220,8 @@ class Entity implements ServiceLocatorAwareInterface
      * Return the Unique ID assigned to this entity
      * @return string
      */
-    public function getUniqueId() {
+    public function getUniqueId()
+    {
         return $this->_unique_id;
     }
 
@@ -210,27 +229,28 @@ class Entity implements ServiceLocatorAwareInterface
      * Return the last updated date of this Entity (accurate to when it was loaded/reloaded)
      * @return string A date-time string
      */
-    public function getUpdatedAt() {
+    public function getUpdatedAt()
+    {
         return $this->_updated_at;
     }
 
     /**
-     * Return the parent Entity ID of this Entity, or null if none specified.
-     * @return int|null
+     * Return the parent Entity ID of this Entity, or NULL if none specified.
+     * @return int|NULL
      */
-    public function getParentId() {
+    public function getParentId()
+    {
         return $this->_parent_id;
     }
 
     /**
      * Gets a value from this Entity.
-     *
      * @param string $key The attribute code to retrieve the value for
-     * @param mixed|null $default The value to return if nothing is set (the value is null)
-     * @return string|array|null The value, or null/default if none specified
+     * @param mixed|NULL $default The value to return if nothing is set (the value is NULL)
+     * @return string|array|NULL The value, or NULL/default if none specified
      * @throws \Magelink\Exception\MagelinkException If the attribute was not loaded for this Entity
      */
-    public function getData($key, $default=null)
+    public function getData($key, $default = NULL)
     {
         if (!isset($this->_attributesMap[$key])) {
             if (is_object($key)) {
@@ -257,131 +277,135 @@ class Entity implements ServiceLocatorAwareInterface
 
     /**
      * Fetch all set data as an associative array
-     * @return array
+     * @return array $allSetData
      */
     public function getAllSetData()
     {
-        $ret = array();
-        $invMap = array_flip($this->_attributesMap);
-        foreach($this->_data as $k=>$v){
-            $ret[$invMap[$k]] = $v;
+        $data = array();
+        $invertedMap = array_flip($this->_attributesMap);
+
+        foreach ($this->_data as $key=>$value) {
+            $data[$invertedMap[$key]] = $value;
         }
-        return $ret;
+        return $data;
     }
 
-    public function hasAttribute($key){
+    /**
+     * @param string $key
+     * @return bool $hasAttribute
+     */
+    public function hasAttribute($key)
+    {
         return isset($this->_attributesMap[$key]);
     }
 
-    public function getAttributes(){
+    /**
+     * @return array $attributes
+     */
+    public function getAttributes()
+    {
         return $this->_attributes;
     }
 
     /**
      * Same as getArrayCopy except returns only the attributes specified and in the order in which they were specified.
-     *
-     * @param array $attributes A list of attribute codes
-     * @return array
+     * @param array $attributes  A list of attribute codes
+     * @return array $specificAttributes
      */
-    public function getArrayCopySorted($attributes){
-        $ret = array();
+    public function getArrayCopySorted($attributes)
+    {
+        $specificAttributes = array();
         foreach($attributes as $code){
-            $ret[$code] = $this->getData($code);
+            $specificAttributes[$code] = $this->getData($code);
         }
-        return $ret;
+        return $specificAttributes;
     }
 
     /**
      * Get a array for attributes mapping value (Mainly for Zend/Form/Form)
-     * @return array
+     * @return array $attributesByValueCode
      */
     public function getArrayCopy()
     {
-        $results = array();
+        $attributes = array();
         foreach ($this->_attributes as $value) {
-            $results[$value['code']] = $this->getData($value['code']);
+            $attributes[$value['code']] = $this->getData($value['code']);
         }
 
-        return $results;
+        return $attributes;
     }
 
     /**
      * Get a array for attributes mapping value (Mainly for Zend/Form/Form)
-     * @return array
+     * @return array $allAttributes
      */
     public function getFullArrayCopy()
     {
-        $results = $this->getArrayCopy();
+        $allAttributes = $this->getArrayCopy();
 
         $staticFields = $this->getServiceLocator()->get('entityConfigService')->getStaticFields();
         foreach ($staticFields as $staticField) {
             $method = 'get'.str_replace(' ', '', ucwords(str_replace('_', ' ', strtolower($staticField))));
-            $results[$staticField] = $this->$method();
+            $allAttributes[$staticField] = $this->$method();
         }
 
-        return $results;
+        return $allAttributes;
+    }
+
+    /**
+     * Forcible changes the loaded node ID, for situations where it is not known upon creation
+     * @param $nodeId
+     */
+    public function setLoadedNodeId($nodeId)
+    {
+        $this->_loadedFromNode = $nodeId;
     }
 
     /**
      * Return the node ID that initially loaded this Entity. May be 0 in rare cases.
      * @return int
      */
-    public function getLoadedNodeId(){
+    public function getLoadedNodeId()
+    {
         return $this->_loadedFromNode;
     }
 
     /**
-     * Forcible changes the loaded node ID, for situations where it is not known upon creation
-     * @param $node_id
-     */
-    public function setLoadedNodeId($node_id){
-        $this->_loadedFromNode = $node_id;
-    }
-
-    /**
      * Resolve a foreign-key relationship. Specified attribute must be Entity type.
-     * 
-     * @param string $attribute_code
+     * @param string $attributeCode
      * @param int|string $entityType
      * @return Entity
      */
-    public function resolve($attribute_code, $entityType = NULL)
+    public function resolve($attributeCode, $entityType = NULL)
     {
-        /** @var \Entity\Service\EntityService $entityService */
-        $entityService = $this->getServiceLocator()->get('entityService');
-
-        if (!array_key_exists($attribute_code, $this->_resolveCache)) {
-            $id = $this->getData($attribute_code, FALSE);
+        if (!array_key_exists($attributeCode, $this->_resolveCache)) {
+            $id = $this->getData($attributeCode, FALSE);
             if (!$id) {
-                $this->_resolveCache[$attribute_code] = NULL;
+                $this->_resolveCache[$attributeCode] = NULL;
             }else{
-                $this->_resolveCache[$attribute_code] = $entityService->loadEntityId($this->_loadedFromNode, $id);
+                $this->_resolveCache[$attributeCode] = $this->getEntityService()
+                    ->loadEntityId($this->_loadedFromNode, $id);
             }
         }
 
-        return $this->_resolveCache[$attribute_code];
+        return $this->_resolveCache[$attributeCode];
     }
 
-    protected $_parentCache = false;
-
     /**
-     * Get parent
-     * @return \Entity\Entity
+     * @return \Entity\Entity $this->_parentCache
      */
     public function getParent()
     {
-        if($this->_parentCache !== false){
-            return $this->_parentCache;
+        if ($this->_parentCache === FALSE) {
+            $this->_parentCache = $this->getEntityService()
+                ->loadEntityId($this->_loadedFromNode, $this->getParentId());
         }
-        return ($this->_parentCache = $this->getServiceLocator()->get('entityService')
-            ->loadEntityId($this->_loadedFromNode, $this->getParentId()));
-    }
 
-    protected $_childrenCache = array();
+        return $this->_parentCache;
+    }
 
     /**
      * Loads all children of the given type for this Entity.
-     *
      * @param string $entityType
      * @return \Entity\Entity[]
      */
