5ec5e1d920b022da1926d90e45fed5ea4c7c415b
gelysis <andreas@gelysis.net>
2016-08-16 10:16:13 +1200

    Syntax cleanup

--- Entity/src/Entity/Action.php
+++ Entity/src/Entity/Action.php
@@ -1,9 +1,12 @@
 <?php
-
-/* 
- * Copyright (c) 2014 Lero9 Limited
- * All Rights Reserved
- * This software is subject to our terms of trade and any applicable licensing agreements.
+/**
+ * Represents an instance of a Magelink Entity Action.
+ * @category Entity
+ * @package Entity
+ * @author Matt Johnston
+ * @author Andreas Gerhards <andreas@lero9.co.nz>
+ * @copyright Copyright (c) 2014 LERO9 Ltd.
+ * @license Commercial - All Rights Reserved
  */
 
 namespace Entity;
@@ -12,17 +15,28 @@ use Magelink\Exception\MagelinkException;
 use Zend\ServiceManager\ServiceLocatorAwareInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
-/**
- * Represents an instance of a Magelink Entity Action.
- */
-class Action  {
 
+class Action
+{
+
+    /** @var int $this->_id */
     protected $_id;
+    /** @var Entity $this->_entity */
     protected $_entity;
+    /** @var string $this->_type */
     protected $_type;
+    /** @var array $this->_data */
     protected $_data = array();
 
-    public function init($id, Entity $entity, $type, $data){
+
+    /**
+     * @param int $id
+     * @param Entity $entity
+     * @param string $type
+     * @param array $data
+     */
+    public function init($id, Entity $entity, $type, $data)
+    {
         $this->_id = $id;
         $this->_entity = $entity;
         $this->_type = $type;
@@ -30,38 +44,53 @@ class Action  {
     }
 
     /**
-     * @return int
+     * @return int $this->_id
      */
-    public function getId(){
+    public function getId()
+    {
         return $this->_id;
     }
 
     /**
-     * @return Entity
+     * @return Entity $this->_entity
      */
-    public function getEntity(){
+    public function getEntity()
+    {
         return $this->_entity;
     }
 
     /**
-     * @return string
+     * @return string $this->_type
      */
-    public function getType(){
+    public function getType()
+    {
         return $this->_type;
     }
 
-    public function getData($key=null){
-        if($key === null){
-            return $this->_data;
-        }else if(isset($this->_data[$key])){
-            return $this->_data[$key];
+    /**
+     * @param mixed $key
+     * @return array|mixed|NULL
+     */
+    public function getData($key = NULL)
+    {
+        if (is_null($key)) {
+            $data = $this->_data;
+        }elseif (isset($this->_data[$key])) {
+            $data = $this->_data[$key];
         }else{
-            return null;
+            $data = NULL;
         }
+
+        return $data;
     }
 
-    public function hasData($key){
+    /**
+     * @param mixed $key
+     * @return bool $hasData
+     */
+    public function hasData($key)
+    {
         return isset($this->_data[$key]);
     }
-    
+
 }
